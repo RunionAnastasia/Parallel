8c37ad468c7763639c71f1226586dd66153dfcc0
Anastasia <arunion1@students.towson.edu>
2024-11-19 05:23:53 -0500

    landing page, profile view, and profile edit

--- parallel/src/App.jsx
+++ parallel/src/App.jsx
@@ -26,20 +26,28 @@
 
 import { useState } from 'react';
 import './App.css';
+import LandingPage from './LandingPage';
 import SignUpForm from './SignUpForm';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './login';
+import Header from './Header';
+import ProfileView from './ProfileView';
+
 
 function App() {
   return (
+ 
     <BrowserRouter> 
+       <Header/> 
       <Routes> 
-        <Route path='/' element={<SignUpForm />} />
+        <Route path='/' element={<LandingPage />} /> 
         <Route path='/signup' element={<SignUpForm />} />
         <Route path='/login' element={<Login />} /> 
+        <Route path='/profileview' element={<ProfileView />} />
       </Routes>
     </BrowserRouter> 
   );
 }
 
 export default App;
+
